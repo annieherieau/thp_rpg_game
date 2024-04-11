f950f53ffb68456d1ec5e22529434ec9c5ce1e6a
annieherieau <annie.herieau@gmail.com>
2024-04-11 20:05:38 +0200

    collapse

--- module/app.js
+++ module/app.js
@@ -3,9 +3,8 @@ import { Game } from "./game.js";
 // lancement de l'application
 export function launchApp(){
   document.getElementById('launchApp').classList.toggle('collapse');
-  // document.getElementById('settings').classList.toggle('collapse');
+  document.getElementById('settings').classList.toggle('collapse');
   // rpgGame.getInput(document.getElementById("userInput").value);
   const rpgGame = new Game();
   rpgGame.startGame();
-  document.getElementById('launchApp').classList.toggle('collapse');
 }
